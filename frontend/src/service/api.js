e748a922f86d6dba6ae87be9e30a6420daa2cdb4
Direct25 <aryankushwaha2536@gmail.com>
2025-07-19 23:08:23 +0530

    minor bug fixed

--- frontend/src/service/api.js
+++ frontend/src/service/api.js
@@ -17,7 +17,6 @@ import axios from 'axios';
 
 const API_URL = process.env.REACT_APP_API_URL || "http://localhost:3001/api";
 
-console.log("API_URL:", API_URL);
 
 export const getSignedUrls = async (files) => {
     try {
